5638bae40aa075353ffe6594a00d6dfc47515a49
ricardosantosfc <58186574+ricardosantosfc@users.noreply.github.com>
2025-07-07 23:26:52 +0100

    Revert "."
    This reverts commit f8c5ee08db0c350e17cfac24f79dabca791e292e.

--- models/session.js
+++ models/session.js
@@ -5,7 +5,10 @@ const Schema = mongoose.Schema;
 //express stores id as a string, so have to explicitly declare it in the schema as a string, otherwise it assumes its an objectid
 
 const SessionSchema = new Schema({ 
-   
+    _id: {
+        type: String,
+        required: true
+    },
     expires: { type: Date, required: true },
     session: {
         cookie: {type:Object},

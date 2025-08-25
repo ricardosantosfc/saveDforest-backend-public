b3be96014c9e9d32a2aeea9fd3dd984fae0b7314
ricardosantosfc <58186574+ricardosantosfc@users.noreply.github.com>
2025-08-25 17:10:46 +0100

    samController: rename nextScene

--- controllers/samController.js
+++ controllers/samController.js
@@ -3,7 +3,7 @@ const saveData = require('../models/saveData');
 const requireAuth = require('../middlewares/requireAuth');
 
 //req.body.scene = scene that the sam is referring to. 
-//req.body.next_scene = scene that comes after sam has been succesefuly recieved by backend, 
+//req.body.nextScene = scene that comes after sam has been succesefuly recieved by backend, 
 //which becomes current scene in the saved data (so the game knows what scene should be loaded)
 //only triggered during first playthrough
 exports.sam_post = [requireAuth, async function(req, res,next){
@@ -12,7 +12,7 @@ exports.sam_post = [requireAuth, async function(req, res,next){
         typeof req.body.scene !== 'number' ||
         typeof req.body.arousal !== 'number' ||
         typeof req.body.valence !== 'number' ||
-        typeof req.body.next_scene !== 'number' ||
+        typeof req.body.nextScene !== 'number' ||
         typeof req.body.score !== 'number'
     ) {
         return res.status(400).send({ success: false, message: "bad request" });
@@ -26,7 +26,7 @@ exports.sam_post = [requireAuth, async function(req, res,next){
             await sam.save() 
             await saveData.updateOne(
                 { user: req.session.user },
-                { $set: { currentScene: req.body.next_scene, 
+                { $set: { currentScene: req.body.nextScene, 
                     score: req.body.score} },            
             ); 
             res.status(200).send({message: "sam successefully stored"});
@@ -40,13 +40,13 @@ exports.sam_post = [requireAuth, async function(req, res,next){
 //updates savedata during subsequent playthroughs. 
 exports.update_score_sam = [requireAuth, async function(req, res,next){
 
-    if (typeof req.body.next_scene !== 'number' || typeof req.body.score !== 'number') {
+    if (typeof req.body.nextScene !== 'number' || typeof req.body.score !== 'number') {
         return res.status(400).send({ success: false, message: "bad request" });
     }   
         try{
             await saveData.updateOne(
                 { user: req.session.user },
-                { $set: { currentScene: req.body.next_scene, 
+                { $set: { currentScene: req.body.nextScene, 
                     score: req.body.score} }
             ); 
             res.status(200).send({message: "score successfully updated"});
